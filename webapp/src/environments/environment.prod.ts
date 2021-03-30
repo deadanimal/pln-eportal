26bf451e3dbb5d9a71aa28ac05a4ee3defba88e8
afizihusaini <afizihusaini@pipeline-network.com>
2021-03-30 14:31:05 +0800

    Build 32 webapp

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: "https://pln-eportal-api.pipe.my/",
+  // baseUrl: "https://pln-eportal-api.pipe.my/",
+  baseUrl: "http://api.planetariumnegara.gov.my/",
 };
