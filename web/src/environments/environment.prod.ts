02d93798b43666379eadb1303aca820434df757e
afizihusaini <afizihusaini@pipeline-network.com>
2021-03-31 16:42:26 +0800

    Build 73 web

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  // assetUrl: "https://portal.planetarium.prototype.com.my/assets/",
-  // baseUrl: "https://pln-eportal-api.pipe.my/",
-  // portalUrl: "https://portal.planetarium.prototype.com.my/#/",
-  assetUrl: "http://103.245.91.181/assets/",
-  baseUrl: "http://api.planetariumnegara.gov.my/",
-  portalUrl: "http://103.245.91.181/#/",
+  assetUrl: "https://portal.planetarium.prototype.com.my/assets/",
+  baseUrl: "https://pln-eportal-api.pipe.my/",
+  portalUrl: "https://portal.planetarium.prototype.com.my/#/",
+  // assetUrl: "http://103.245.91.181/assets/",
+  // baseUrl: "http://api.planetariumnegara.gov.my/",
+  // portalUrl: "http://103.245.91.181/#/",
   reCaptchaSiteKey: "6Lcw0lwaAAAAAFqNAEkV3dYsUewndOU2Uzx8SW3S",
   reCaptchaSecretKey: "6Lcw0lwaAAAAAP-5dBKk8w-qL2RzyvfM2KSI_w1q",
 };
